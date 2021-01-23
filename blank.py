ee5caefce218f3629d85b71ca3a262931bdd1c50
a.hautot <alexander.vi@gmail.com>
2021-01-23 22:04:34 +1100

    added non-example test

---
+++ blank.py
@@ -0,0 +1,4 @@
+class this_class:
+
+    def func(x):
+        return x + 1
--- bowling_test.py
+++ bowling_test.py
@@ -1,5 +1,14 @@
-def func(x):
-    return x + 1
+
+from blank import this_class
+
+from bowling import bowling_game
 
 def test_answer():
-    assert func(3) == 4
+    #this = this_class()
+    assert this_class.func(3) == 4
+
+def test_calculate_standard_frame():
+    frame = 0
+    three_gutters = [[0],[0],[0]]
+    result = bowling_game.calculate_standard_frame(frame, three_gutters)
+    assert result == 0
