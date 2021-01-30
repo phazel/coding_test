b70f5e727cbd4eaf1a75d95e4320f8ccf3f6299d
Alex <alexander.vi@gmail.com>
2021-01-30 20:48:20 +1100

    added spare and strike test

--- bowling_test.py
+++ bowling_test.py
@@ -7,8 +7,23 @@ def test_answer():
     #this = this_class()
     assert this_class.func(3) == 4
 
-def test_calculate_standard_frame():
+#
+def test_calculate_open_frame():
     frame = 0
-    three_gutters = [[0],[0],[0]]
-    result = bowling_game.calculate_standard_frame(frame, three_gutters)
+    two_gutters = [[0,0]]
+    result = bowling_game.calculate_standard_frame(frame, two_gutters)
     assert result == 0
+
+# calculates a frame with a strike
+def test_calculate_strike_frame():
+    frame = 0
+    strike = [[10],[3,3]]
+    result = bowling_game.calculate_standard_frame(frame, strike)
+    assert result == 16
+
+# calculates a spare score
+def test_calculate_spare_frame():
+    frame = 0
+    spare = [[3,7],[3,3]]
+    result = bowling_game.calculate_standard_frame(frame, spare)
+    assert result == 13
